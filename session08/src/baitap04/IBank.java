5d3678cc9871a866998a2542a750e592c9c92bad
dinhtrang23 <dinhtrang2315@gmail.com>
2025-05-28 14:55:27 +0900

    Add files via upload

---
+++ session08/src/baitap04/IBank.java
@@ -0,0 +1,6 @@
+package baitap04;
+
+interface IBank {
+    void deposit(double amount);
+    void withdraw(double amount);
+}
---
+++ session08/src/baitap06/Book.java
@@ -0,0 +1,24 @@
+package baitap06;
+
+    class Book {
+        private String title;
+        private String author;
+        private String isbn;
+        private int year;
+
+        public Book(String title, String author, String isbn, int year) {
+            this.title = title;
+            this.author = author;
+            this.isbn = isbn;
+            this.year = year;
+        }
+
+        public String getDetails() {
+            return "Tiêu đề: " + title + ", Tác giả: " + author + ", ISBN: " + isbn + ", Năm: " + year;
+        }
+
+        public String getIsbn() {
+            return isbn;
+        }
+    }
+
