9476d3982008d0e30147515115de4cd3321b531f
dinhtrang23 <dinhtrang2315@gmail.com>
2025-07-02 23:32:07 +0900

    Add files via upload

---
+++ MD2_Exam_DinhThiXuanTrang/src/until/ConnectionDB.java
@@ -0,0 +1,15 @@
+package until;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+    private static final String DRIVER = "jdbc:mysql://localhost:3306";
+    private static final String URL = "com.mysql.cj.jdbc.Driver";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "trang!";
+
+    public static Connection openConnection() {
+
+    }
+}
